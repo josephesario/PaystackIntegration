6566db4f8ddd7b1ab01e57511cae0df4c428c7ca
jose rodolfo Esapa Riochi <72030168+josephesario@users.noreply.github.com>
2024-08-24 23:22:45 +0000

    DonateViewModel

--- Model/DonateViewModel.cs
+++ Model/DonateViewModel.cs
@@ -2,5 +2,8 @@
 {
 	public class DonateViewModel
 	{
+		public string Name { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public int Amount { get; set; }
 	}
 }
